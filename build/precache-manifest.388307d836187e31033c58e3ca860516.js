baca3ea9bac4a1d3f8a4757bf346497bc1cc50a9
manuelbiolatiri <manuellaide49@gmail.com>
2021-01-21 00:25:30 +0000

    build

---
+++ build/precache-manifest.388307d836187e31033c58e3ca860516.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85169fe9134e5e5a67fd1b276a0e9173",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4524ef980e6c02587959",
+    "url": "/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "388b30a855e110084e5f",
+    "url": "/static/css/main.4af3bfea.chunk.css"
+  },
+  {
+    "revision": "4524ef980e6c02587959",
+    "url": "/static/js/2.2fb375ba.chunk.js"
+  },
+  {
+    "revision": "0b298e2e029bf367fa22865d4c53d07e",
+    "url": "/static/js/2.2fb375ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "388b30a855e110084e5f",
+    "url": "/static/js/main.259f6eaa.chunk.js"
+  },
+  {
+    "revision": "b68e2f13b8d07dcde4c4",
+    "url": "/static/js/runtime-main.ad0f43f1.js"
+  }
+]);
